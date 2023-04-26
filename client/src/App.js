fcdf0df1b5f504cac08eb2c60c73d049fefb8a21
AlekhyaPasupuleti <alekhyasavithri.pasupuleti@sjsu.edu>
2023-04-25 20:50:58 -0700

    Service update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Sidebar from "./components/Sidebar";
 	import { BrowserRouter as Router, Routes, Route}
 		from 'react-router-dom';
 // import LoginComponent from './Login/LoginComponent';
@@ -13,13 +14,22 @@ import Contact from './pages/Contacts/contact';
 import MemberHomePage from './pages/MemberHomePage/member';
 import HCMember from './pages/HCMemberHomePage/hcmember';
 import LoginComponent from './pages/Login/login';
+import { GymEnrollment } from './pages/GymEnrollment';
+import { LogHours } from './pages/LogHours';
+import ActivitiesChart from './pages/ActivitiesChart';
+import ClassSchedule from './pages/ClassSchedule';
 function App() {
   return (
     
     <Router>
     <Navbar />
+    <Sidebar />
     <Routes>
       <Route  path='/'  element={<Home />} />
+      <Route path='/gymenrollment' element={<GymEnrollment/>} />
+      <Route path='/loghours' element={<LogHours/>} />
+      <Route path='/activitieschart' element={<ActivitiesChart/>} />
+      <Route path='/classSchedule' element={<ClassSchedule/>} />
       <Route path='/service' element={<Service/>} />
       <Route path='/contact' element={<Contact/>} />
       <Route path='/classes' element={<Classes/>} />
