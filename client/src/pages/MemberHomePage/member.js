f7b86c623fafe853d9836ee9f2ef4b875c988f13
pchennam1998 <pavansai.chennam@sjsu.edu>
2023-04-30 11:13:20 -0700

    Logout in Member Homepage

--- client/src/pages/MemberHomePage/member.js
+++ client/src/pages/MemberHomePage/member.js
@@ -4,9 +4,18 @@ import dateFormat from 'dateformat';
 import TreadmillStopwatch from './TreadmillStopwatch';
 import UseStaircase from './UseStaircase';
 import Cycling from './Cycling';
+import { useNavigate } from "react-router-dom";
 
 
 function Member() {
+
+  const navigate = useNavigate();
+
+	const handleLogout = () => {
+		console.log("Logout came")
+		localStorage.removeItem('auth');
+		navigate('/login');
+  }
 
     const initialValues = []
     const [pagedata,setPagedata]=useState(initialValues);
@@ -40,6 +49,7 @@ function Member() {
     <h1>Member Dashboard</h1><br></br>
       {/* <h2>{emailAddress}</h2> */}
       <div>
+      <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
       <h3 color='#'>Class Schedule for a week</h3>
       <img src='https://fitpage.in/wp-content/uploads/2021/10/Article_Banner-1-1.jpg' hegiht="350" width="450"></img>
       <h4>{services} at {location}</h4>
