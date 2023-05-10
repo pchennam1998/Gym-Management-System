2a77811bb0a92e5dadedf582806ddd43094adfc5
pchennam1998 <pavansai.chennam@sjsu.edu>
2023-05-10 13:27:06 -0700

    Frontend Page data modify

--- client/src/pages/Classes/classes.js
+++ client/src/pages/Classes/classes.js
@@ -9,18 +9,24 @@ function Clsses() {
 
   const data = [
     {
-      location: 'Santa Clara',
-      services: 'Threadmill',
+      location: 'San Jose',
+      services: 'Treadmill, Weightlifting and Staircase',
       classDay: "Monday, Wednesday and Friday",
       startTime: new Date('T10:00:00'),
-      endTime: new Date('T10:00:00')
+      endTime: new Date('T10:00:00'),
+      classTime: "6:00 AM to 10:00 PM",
+      image: "https://media.istockphoto.com/id/1322158059/photo/dumbbell-water-bottle-towel-on-the-bench-in-the-gym.jpg?s=612x612&w=0&k=20&c=CIdh6LPGwU6U6lbvKCdd7LcppidaYwcDawXJI-b0yGE=",
+      text: "The gym in San Jose is a popular destination for fitness enthusiasts in the area. It offers a wide range of equipment and facilities for various types of workouts, including weightlifting, cardio, and group fitness classes. The gym is well-maintained and spacious, allowing visitors to exercise comfortably and safely.Aside from the workout facilities, the gym also has knowledgeable and friendly staff who can help visitors achieve their fitness goals. They offer personalized workout plans, nutritional advice, and support to keep visitors motivated and on track. The gym also hosts various fitness events throughout the year, including fitness challenges, seminars, and social gatherings.Overall, the gym in San Jose is a great place for people looking to improve their fitness, whether they are just starting out or are experienced athletes. With its top-notch equipment, supportive staff, and welcoming atmosphere, it is a great choice for anyone looking to get in shape and lead a healthier lifestyle."
     },
     {
-		location: 'Sant Jose',
-		services: 'Weight Lifting',
+		location: 'Santa Clara',
+		services: 'Treadmill, Weightlifting and Staircase',
 		classDay: 'Tuesday, Thursday and Saturday',
 		startTime: new Date('T10:00:00'),
-		endTime: new Date('T10:00:00')
+		endTime: new Date('T10:00:00'),
+    classTime: "6:00 AM to 8:00 PM",
+    image: "https://www.hussle.com/blog/wp-content/uploads/2020/12/Gym-structure-1080x675.png",
+    text: "The gym in Santa Clara is a well-equipped and spacious fitness center that offers a wide range of workout options to its members. With state-of-the-art equipment and facilities, the gym provides an ideal setting for people looking to improve their health and fitness levels. The gym has a dedicated team of professional trainers who are always on hand to help members achieve their fitness goals. From weightlifting and cardio to yoga and group classes, the gym has something for everyone. In addition to its fitness offerings, the gym also provides nutritional advice and meal plans to help members maintain a healthy lifestyle. The gym in Santa Clara has a welcoming and friendly environment that makes it easy for members to feel comfortable and motivated. Whether you're a beginner or an experienced fitness enthusiast, the gym provides a supportive community that will help you achieve your goals. With its convenient location and flexible membership options, the gym in Santa Clara is a great choice for anyone looking to get fit and stay healthy."
     }
     // add more exercise objects as needed
   ];
@@ -42,8 +48,10 @@ function Clsses() {
       {exercise &&
         <div className="grid-block">
           <h2><b>{exercise.services}</b></h2>
-          <p><b>Class Days:</b> Every Weekday </p><br></br>
-          <p><b>In/Out Time:</b> 06:00 AM - 10:00 PM</p><br></br>
+          <p><b>Class Days:</b> Everyday  </p><br></br>
+          <p><b>In/Out Time:</b> {exercise.classTime}</p><br></br>
+          <p><img src={exercise.image} width="800px" height="400px"/></p>
+          {exercise.text}
         </div>
       }
     </div>
--- client/src/pages/Service/service.js
+++ client/src/pages/Service/service.js
@@ -3,15 +3,15 @@ import './service1.css';
 
 function CardGrid() {
   const cards = [
-    { title: 'Threadmill', image: 'https://fitpage.in/wp-content/uploads/2021/10/Article_Banner-1-1.jpg', description: 'This is the description for card 1.' },
-    { title: 'Weight Lifting', image: 'https://fitpage.in/wp-content/uploads/2021/11/Article_Banner-2-13-1024x576.jpg', description: 'This is the description for card 2.' },
-    { title: 'Staircase', image: 'https://www.liberty.edu/campusrec/reccenters/wp-content/uploads/sites/13/2020/04/bruno-nascimento-PHIgYUGQPvU-unsplash-1024x683.jpg', description: 'This is the description for card 3.' },
-    { title: 'Threadmill', image: 'https://fitpage.in/wp-content/uploads/2021/10/Article_Banner-1-1.jpg', description: 'This is the description for card 1.' },
-    { title: 'Weight Lifting', image: 'https://fitpage.in/wp-content/uploads/2021/11/Article_Banner-2-13-1024x576.jpg', description: 'This is the description for card 2.' },
-    { title: 'Staircase', image: 'https://www.liberty.edu/campusrec/reccenters/wp-content/uploads/sites/13/2020/04/bruno-nascimento-PHIgYUGQPvU-unsplash-1024x683.jpg', description: 'This is the description for card 3.' }
+    { title: 'Treadmill', image: 'https://fitpage.in/wp-content/uploads/2021/10/Article_Banner-1-1.jpg', description: '    The treadmill is a relatively easy piece of exercise equipment to use. ' },
+    { title: 'Weight Lifting', image: 'https://fitpage.in/wp-content/uploads/2021/11/Article_Banner-2-13-1024x576.jpg', description: 'It is a popular form of exercise for people who want to build strength, muscle mass, and endurance.' },
+    { title: 'Staircase', image: 'https://www.liberty.edu/campusrec/reccenters/wp-content/uploads/sites/13/2020/04/bruno-nascimento-PHIgYUGQPvU-unsplash-1024x683.jpg', description: 'A staircase is a set of steps that connect two or more floors in a building or structure.' },
+    { title: 'Treadmill', image: 'https://www.liberty.edu/campusrec/wp-content/uploads/sites/191/2021/07/Nikki-e1627310081772.jpg', description: 'A treadmill is a piece of exercise equipment that is used for running or walking indoors.' },
+    { title: 'Weight Lifting', image: 'https://www.powerhouse-fitness.co.uk/blog/wp-content/uploads/2020/12/Blog_olympic_weightlifting-7.jpg', description: 'Weightlifting is a sport that involves lifting weights to compete against other athletes or oneself.' },
+    { title: 'Staircase', image: 'https://i.ytimg.com/vi/ExB2nnyKrd8/hqdefault.jpg', description: 'Staircase exercise, also known as stair climbing or step aerobics, is a form of cardiovascular exercise that involves repeatedly stepping up and down a set of stairs or steps.' }
   ];
 
-  const chunkSize = 2; // Number of cards per row
+  const chunkSize = 3; // Number of cards per row
   const chunks = cards.reduce((resultArray, item, index) => { 
     const chunkIndex = Math.floor(index / chunkSize);
 
